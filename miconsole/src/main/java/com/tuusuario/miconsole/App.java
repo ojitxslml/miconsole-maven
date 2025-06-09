7acaaecb4a771fb4ec7e6db717f852223cf8d4ec
Juan Pablo Urra Jara <97376112+ojitxslml@users.noreply.github.com>
2025-06-09 23:45:26 +0000

    creación de proyecto con Maven

---
+++ miconsole/src/main/java/com/tuusuario/miconsole/App.java
@@ -0,0 +1,13 @@
+package com.tuusuario.miconsole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
