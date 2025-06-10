e14128f2c936c4ae45c4f41d7c5317186e91ba90
Juan Pablo Urra Jara <97376112+ojitxslml@users.noreply.github.com>
2025-06-10 00:19:35 +0000

    add Persona class

--- miconsole/src/main/java/com/tuusuario/miconsole/App.java
+++ miconsole/src/main/java/com/tuusuario/miconsole/App.java
@@ -1,13 +1,18 @@
 package com.tuusuario.miconsole;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        String texto = " Hola Maven en la nube ";
+        System.out.println("¿Texto está en blanco?: " + StringUtils.isBlank(texto));
+        System.out.println("Texto sin espacios: '" + StringUtils.trim(texto) + "'");
+
+        Persona persona = new Persona("Brayan", 30);
+        String json = new Gson().toJson(persona);
+        System.out.println("Persona en JSON: " + json);
     }
 }
---
+++ miconsole/src/main/java/com/tuusuario/miconsole/Persona.java
@@ -0,0 +1,10 @@
+package com.tuusuario.miconsole;
+public class Persona {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
